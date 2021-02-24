1030916b54af3cc1fcf0baeae128f500766d0701
linyinfu <15058974874@163.com>
2021-02-24 15:38:35 +0800

    提交

---
+++ public/ip.js
@@ -0,0 +1,8 @@
+// window.ip='http://192.168.3.104:7000/api';
+// window.ip='https://192.168.3.110:7000/api';
+// window.ip='http://192.168.3.102:8083/api';
+// window.ip='http://192.168.3.4:8080/api';
+window.ip='http://192.168.3.4:8080/api';
+// window.ip='/api';
+// window.ip='http://192.168.3.115:7000/api';
+// window.ip='https://192.168.3.30/api';
